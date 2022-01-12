a2e14909148a12171b8e38205a3a39144c0108b9
ZaharMachiavelli <Mark.Zaharenko@ya.ru>
2022-01-12 15:48:25 +0500

    wip

--- src/db.js
+++ src/db.js
@@ -9,7 +9,7 @@ import john from "@/assets/john.jpg"
 const data = [
     {name:"Великий Гэтсби", genre:"drama", relation: "emotions", single: true, year: "10", actors: ["Леонардо Ди Каприо", "Тоби Магуайр"], mark: 7.9, image: gatsby} ,
     {name:"Борат", genre:"comedy", relation: "chill", single: false, year: "00", actors: ["Саша Барон Коэн",], mark: 6.7, image: borat },
-    {name:"Властелин Колец: Братство кольца", genre:"travel", relation: "think", single: false, year: "00", actors: ["Элайджа Вуд",], mark: 8.6, image: sanya },
+    {name:"Властелин Колец: Братство кольца", genre:"travel", relation: "think", single: false, year: "00", actors: ["Элайджа Вуд"], mark: 8.6, image: sanya },
     {name:"Сонная Лощина", genre:"detective", relation: "emotions", single: true, year: "90", actors: ["Джонни Депп",], mark: 7.9, image: sonny },
     {name:"Ла-Ла Ленд", genre:"musicle", relation: "sad", single: true, year: "10", actors: ["Райан Гослинг", "Эмма Стоун"], mark: 7.9, image: lala },
     {name:"Пила: Игра на выживание", genre:"horror", relation: "emotions", single: false, year: "00", actors: ["Ли Уоннелл",], mark: 7.8, image: pilaz },
